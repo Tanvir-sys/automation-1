69b99d3cf0e37e676d926f86719ca2a1e254a55c
Md. Tanvir Hossain <163hossaintanvir@gmail.com>
2023-07-04 21:00:51 +0600

    Navigate by locator

---
+++ src/test/java/AutomationTesting_1/auttomation_1/LocatorExample.java
@@ -0,0 +1,14 @@
+package AutomationTesting_1.auttomation_1;
+
+import org.testng.annotations.Test;
+
+public class LocatorExample extends BaseDriver {
+	@Test
+	public void testlocators() {
+		
+		driver.get("");
+		Thread.sleep(5000);
+		
+	}
+
+}
