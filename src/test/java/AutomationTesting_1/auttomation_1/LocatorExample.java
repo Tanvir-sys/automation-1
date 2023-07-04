4fb5283ac9333cb772f2177adf3cc98ce355d24f
Md. Tanvir Hossain <163hossaintanvir@gmail.com>
2023-07-04 21:19:32 +0600

    Navigate sohoz.com contact-us page and click event by id

--- src/test/java/AutomationTesting_1/auttomation_1/LocatorExample.java
+++ src/test/java/AutomationTesting_1/auttomation_1/LocatorExample.java
@@ -1,14 +1,20 @@
 package AutomationTesting_1.auttomation_1;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class LocatorExample extends BaseDriver {
 	@Test
-	public void testlocators() {
+	public void testlocators() throws InterruptedException  {
 		
-		driver.get("");
+		driver.manage().window().maximize();
+		driver.get("https://www.shohoz.com/contact-us");
 		Thread.sleep(5000);
 		
+		WebElement e=driver.findElement(By.id("purpose"));
+		e.click();
+		Thread.sleep(5000);
 	}
 
 }
