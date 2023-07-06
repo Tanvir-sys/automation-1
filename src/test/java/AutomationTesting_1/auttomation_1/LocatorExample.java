191e7c1e5134023c5e7d155c92ddf105b252ac43
Md. Tanvir Hossain <163hossaintanvir@gmail.com>
2023-07-06 16:55:11 +0600

    automatice write by send key

--- src/test/java/AutomationTesting_1/auttomation_1/LocatorExample.java
+++ src/test/java/AutomationTesting_1/auttomation_1/LocatorExample.java
@@ -12,13 +12,13 @@ public class LocatorExample extends BaseDriver {
 		driver.get("https://www.shohoz.com/contact-us");
 		Thread.sleep(5000);
 		
-//		WebElement e=driver.findElement(By.id("purpose")); // by id
-//		e.click();
-//		Thread.sleep(5000);
+	WebElement e=driver.findElement(By.id("purpose")); // by id
+	e.click();
+		Thread.sleep(5000);
 		
-		WebElement e=driver.findElement(By.name("purpose"));
-		e.click();
-		Thread.sleep(2000);
+//		WebElement e=driver.findElement(By.name("purpose"));
+//		e.click();
+//		Thread.sleep(2000);
 		
 		WebElement s= driver.findElement(By.linkText("Launch"));
 		s.click();
@@ -27,8 +27,11 @@ public class LocatorExample extends BaseDriver {
 		WebElement l=driver.findElement(By.xpath("//*[@id=\"main_wrapper\"]/div/div/header/div[1]/div/div/div/ul/li[1]/a"));
 		l.click();
 		Thread.sleep(10000);
-		
-		
+		WebElement  a=driver.findElement(By.xpath("//*[@id=\"email\"]"));
+		a.sendKeys("abs@lito.com");
+		a=driver.findElement(By.xpath("//*[@id=\"comment\"]"));
+		a.sendKeys("Hey This Is Tanvir from QA Team To check the system");
+		Thread.sleep(20000);
 		
 		
 	}
