afc3fc061c839053c5f246337848959b0157988b
Md. Tanvir Hossain <163hossaintanvir@gmail.com>
2023-07-09 13:05:01 +0600

    Positive Test

--- src/test/java/AutomationTesting_1/auttomation_1/webElementexample.java
+++ src/test/java/AutomationTesting_1/auttomation_1/webElementexample.java
@@ -11,21 +11,30 @@ public class webElementexample  extends BaseDriver{
 		driver.get(Url);
 		
 		WebElement e = driver.findElement(By.id("firstName"));
-		System.out.println("Attribute Value :"  +e.getAttribute("pattern"));
-		String formValue= e.getText();
-		System.out.println(formValue);
 		
-		e.sendKeys("Tanvir Hossain");
-		Thread.sleep(1000);
-	    e.clear();
-	    e.sendKeys("ABM aftabuzamman");
-		Thread.sleep(1000);
-		e.clear();
-		e.sendKeys("abc");
-		Thread.sleep(1000);
+		WebElement q= driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[1]/div[2]/div[2]/div[2]"));
+		System.out.println("Attribute Value :"  +q.getAttribute("pattern"));
+		String formValue= q.getText();
 		
+			System.out.println(formValue);
+				
+				    e.sendKeys("Tanvir Hossain");
+				Thread.sleep(1000);
+			        e.clear();
+				    e.sendKeys("ABM aftabuzamman");
+				Thread.sleep(1000);
+					e.clear();
+					e.sendKeys("Monjurul Hoq");
+				Thread.sleep(1000);
+			if(e.isDisplayed()) {         //  for positive case 
+					e.clear();
+					e.sendKeys("Hello ");
+				Thread.sleep(2000);
+			}else {
+				
+				System.out.println("Email Id Not Locateable .");
+			}
 	
-		
 	}
 	
 	
