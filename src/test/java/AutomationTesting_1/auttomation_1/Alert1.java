1dd54cafb1e9f4d619faf2fd0abebf90a37b31cf
Md. Tanvir Hossain <163hossaintanvir@gmail.com>
2023-07-11 16:12:17 +0600

    all alert button checked

--- src/test/java/AutomationTesting_1/auttomation_1/Alert1.java
+++ src/test/java/AutomationTesting_1/auttomation_1/Alert1.java
@@ -7,24 +7,39 @@ import org.testng.annotations.Test;
 
 public class Alert1 extends BaseDriver{
 	
+	
 	String Url="https://demoqa.com/alerts";
 	@Test
 	public void AlertTest() throws InterruptedException {
+		
 		driver.manage().window().maximize();
 		driver.get(Url);
+		
 		WebElement w = driver.findElement(By.xpath(" //button[@id='alertButton']"));
+		
 		w.click();
 		Thread.sleep(3000);
 		Alert alert= driver.switchTo().alert();
 		alert.accept();
 		Thread.sleep(3000);
-		System.out.println(".........................");
+		System.out.println("..........Print For 1st alert notification...............");
 		
+		WebElement s = driver.findElement(By.id("confirmButton"));
+		s.click();
 		
+		alert.accept();	
+		Thread.sleep(3000);
+		
+		WebElement e =driver.findElement(By.id("confirmResult"));
+		
+		String f=e.getText();
 			
+		System.out.println(f);
 		
 		
-			
+		
+		
+		
 		
 		
 	}
